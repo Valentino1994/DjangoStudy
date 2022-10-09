f7fcb8483112e61ee5b0b7a015249cd23c1e547f
Guell <demua1994@gmail.com>
2022-10-09 21:14:07 +0900

    1009 KEY Setting

--- 1009/first/first/settings.py
+++ 1009/first/first/settings.py
@@ -9,21 +9,40 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
+import pymysql
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+'''
+2. 각 호출자가 기본 매개변수를 전달할 필요가 없도록 환경 변수의 체계 기반 조회를 제공합니다.
+라고 번역하니 나와있는데, 무슨말인지 생각해보니 환경변수를 불러올 수 있는 상태로 세팅한다고
+이해했다. 
+'''
+env = environ.Env(DEBUG=(bool, True))
 
+'''
+3. 환경변수를 읽어올 준비는 마쳤고, 어떤 파일에서 불러올건지 정해줘야 하기 때문에
+나는 '.env'에서 가져올거라고 설정해줬다.
+'''
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
 
+pymysql.install_as_MySQLdb()
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+'''
+4. SECEREY_KEY와 DEBUG에 넣은 값들을 불러올 수 있게 설정
+'''
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&ho3&9ts*5=+4hf+9%us0w(!ujh&ix%vjp!0^6+-5t8%3m==i@"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
