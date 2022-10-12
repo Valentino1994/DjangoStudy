e6c3501405ae715761ff3fd453f6665efb11a7ca
Guell <demua1994@gmail.com>
2022-10-12 21:44:05 +0900

    1012

--- 1011/secondpjt/articles/admin.py
+++ 1011/secondpjt/articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+# Admin 화면을 커스텀할 수 있음
+class ArticleAdmin(admin.ModelAdmin):
+        list_display = ('pk', 'title', 'content', 'updated_at', 'created_at')
+
+# admin site에 Article을 register하겠다.
+admin.site.register(Article, ArticleAdmin)
